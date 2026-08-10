d67de60bc6e568cea3329d4c540f3853143f1076
Diya <diyamaru32@gmail.com>
2026-08-10 08:35:40 +0530

    Add files via upload

---
+++ OOPJ/Lab 3/Operator_Precedance.java
@@ -0,0 +1,41 @@
+// Illustrate the Operator  precedence.[A] 
+// a. 10 + 20 * 30
+// public class Operator_Precedance
+// {
+//     public static void main(String[] args)
+//     {
+//         int result = 10 + 20 * 30;
+//         System.out.println("Result: " + result);
+//     }
+// }
+
+
+//b.100 / 10 * 100 
+// public class Operator_Precedance
+// {
+//     public static void main(String[] args)
+//     {
+//         int result = 100 / 10 * 100;
+//         System.out.print("Result: " + result);
+//     }
+// }
+
+
+//c. 5 * 4 / 4 % 3
+// public class Operator_Precedance
+// {
+//     public static void main(String[] args)
+//     {
+//         System.out.print(5 * 4 / 4 % 3);
+//     }
+// }
+
+
+//d. 100 + 200 / 10 – 3 * 10 
+public class Operator_Precedance
+{
+    public static void main(String[] args)
+    {
+        System.out.print(100 + 200 / 10 - 3 * 10 );
+    }
+}
