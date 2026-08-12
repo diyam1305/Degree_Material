2920c62112f331fd671e2eb6246d9aa78c89acbd
Diya <diyamaru32@gmail.com>
2026-08-12 08:36:37 +0530

    Update Lab 5.sql

--- DBMS/Lab 5.sql
+++ DBMS/Lab 5.sql
@@ -42,3 +42,46 @@ add IFSC_CODE varchar(15)
 --10. Change the size of bname column from varchar(50) to varchar(30). 
 Alter table DEPOSIT_DETAIL
 alter column BRANCH_NAME varchar(30)
+
+
+--PART-B
+
+--11. Rename column adate to aopendate.  
+SP_RENAME 'DEPOSIT_DETAIL.ADATE', AOPENDATE
+
+--12. Delete column aopendate from DEPOSIT_DETAIL table. 
+ALter table DEPOSIT_DETAIL
+drop column AOPENDATE
+
+--13. Rename column cname to customer_name. 
+SP_RENAME 'DEPOSIT_DETAIL.CNAME', 'CUSTOMER_NAME'
+
+--14. Add column country varchar(20). 
+Alter table DEPOSIT_DETAIL
+add COUNTRY varchar(20)
+
+--15. Add column account_type varchar(15). 
+Alter table DEPOSIT_DETAIL
+add ACCOUNT_TYPE varchar(15)
+
+--16. Change data type of pincode from int to bigint. 
+Alter table DEPOSIT_DETAIL
+alter column PINCODE bigint
+
+--17. Delete column account_type.
+Alter table DEPOSIT_DETAIL
+drop column account_type
+
+--18. Rename column amount to balance. 
+SP_RENAME 'DEPOSIT_DETAIL.AMOUNT', 'BALANCE'
+
+--19. Add column status varchar(10). 
+Alter table DEPOSIT_DETAIL
+add STATUS varchar(10)
+
+--20. Change table name deposit_detail to bank_deposit.
+SP_RENAME 'DEPOSIT_DETAIL', 'BANK_DEPOSIT'
+
+
+--PART-A
+
